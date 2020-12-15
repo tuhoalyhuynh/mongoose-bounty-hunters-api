61e1ca105c070fc8510d32bf9f5a430beff199c1
famousPete <pete.macaluso@generalassemb.ly>
2020-12-15 12:18:48 -0500

    now with 100% less .env!!

---
+++ models/Bounty.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const hunterSchema = require('./Hunter').hunterSchema
+
+const bountySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  wantedFor: {
+    type: String,
+    minlength: 10,
+    maxlength: 200
+  },
+  client: String,
+  reward: {
+    type: Number,
+    min: 1000,
+    max: 1000000
+  },
+  ship: String,
+  hunters: [hunterSchema],
+  captured: {
+    type: Boolean,
+    default: false
+  },
+  lastSeen: String
+})
+
+module.exports = mongoose.model('bounties', bountySchema)
