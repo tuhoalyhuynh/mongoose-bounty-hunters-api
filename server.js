124bb233122903dd1417404c73d2fba481ba1862
famousPete <pete.macaluso@generalassemb.ly>
2020-12-15 13:02:25 -0500

    GET and POST routes

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+app.use(express.urlencoded())
+app.use(express.json())
+
 const models = require('./models')
 
 app.get('/', (req, res) => {
   res.send('hello from root')
 })
+
+app.use('/bounties', require('./controllers/bountiesController'))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
